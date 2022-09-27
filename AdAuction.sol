360fb13dcbfd56eb6f912d64279a81b4f5908807
Artem B <artem.bv@pm.me>
2022-09-27 14:35:14 +0300

    refactor: implement error handling

--- AdAuction.sol
+++ AdAuction.sol
@@ -9,52 +9,91 @@ contract AdAuction {
     
     using PriceOracle for uint256;
 
+    error NotOwner();
+    
+    error InvalidAuctionPeriod();
+    error AuctionHasntStartedYet();
+    error AuctionIsOver();
+    error AuctionIsNotOverYet();
+
+    error InvalidMinimumBidRequirement();
+    error BidIsLowerThanMinimum();
+
+    error NoSuchPayer();
+    error HighestBidderCantWithdraw();
+    error BidderAlreadyWithdrew();
+    error BidWithdrawalFailed();
+    
+    error NothingToWithdraw();
+    error OwnerWithdrawalFailed();
+
     struct Payer {
         string name;
         uint256 amount;
         string imageUrl;
         string text;
+        bool withdrew;
     }
 
-    uint256 startAuctionTime;
-    uint256 endAuctionTime;
-    uint256 minimumUsdBid;
+    address public owner;
+    
+    uint256 public startAuctionTime;
+    uint256 public endAuctionTime;
+    uint256 public minimumUsdBid;
 
     address public highestBidderAddr;
-    Payer public highestBidderData;
-    
     mapping (address => Payer) public addressToPayer;
 
     constructor(uint256 _startAuctionTime, uint256 _endAuctionTime, uint256 _minimumUsdBid) {
+        owner = msg.sender;
+
+        if (_endAuctionTime <= _startAuctionTime) revert InvalidAuctionPeriod();
+        if (_minimumUsdBid < 0) revert InvalidMinimumBidRequirement();
+
         startAuctionTime = _startAuctionTime;
         endAuctionTime = _endAuctionTime;
         minimumUsdBid = _minimumUsdBid * 1e18;
     }
 
     function payForAd(string calldata _name, string calldata _imageUrl, string calldata _text) payable external {
-        require(startAuctionTime <= block.timestamp, "AdAuction::payForAd: The auction hasn't started yet");
-        require(endAuctionTime >= block.timestamp, "AdAuction::payForAd: The auction is already over");
+        if (block.timestamp < startAuctionTime) revert AuctionHasntStartedYet();
+        if (block.timestamp > endAuctionTime) revert AuctionIsOver();
+        if (msg.value.getConversionRate() < minimumUsdBid) revert BidIsLowerThanMinimum();
+        if (msg.value <= addressToPayer[highestBidderAddr].amount) revert HigherBidIsAvailable();
         
-        require(msg.value.getConversionRate() > minimumUsdBid, "AdAuction::payForAd: Ad bid is lower than minium auction bin in USD");
-        
-        if (msg.value > highestBidderData.amount) {
-            Payer memory payer = Payer(_name, msg.value, _imageUrl, _text);
-            addressToPayer[msg.sender] = payer;
-            highestBidderAddr = msg.sender;
+        Payer storage payer = addressToPayer[msg.sender];
+        if (payer.amount == 0) {
+            payer = Payer(_name, msg.value, _imageUrl, _text, false);
+        } else {
+            payer.amount += msg.value;
         }
+        highestBidderAddr = msg.sender;
     }
 
-    function withdraw(address receiver) external {
-        require(startAuctionTime <= block.timestamp, "AdAuction::payForAd: The auction hasn't started yet");
-        require(endAuctionTime >= block.timestamp, "AdAuction::payForAd: The auction is already over");
+    // todo: implement logic so you can withdraw not all but a part depending on how long your ad was up
+    function withdrawBid(address receiver) external {
+        if (block.timestamp <= endAuctionTime) revert AuctionIsNotOverYet();
+        if (msg.sender == highestBidderAddr) revert HighestBidderCantWithdraw();
+
         Payer memory payer = addressToPayer[msg.sender];
-        require(payer.amount == 0, "AdAuction::withdraw: No payer available with this address");
-        require(msg.sender != highestBidderAddr, "AdAuction::payForAd: Highest bidder cannot withdraw");
+        if (payer.amount == 0) revert NoSuchPayer();
+        if (payer.withdrew) revert BidderAlreadyWithdrew();
 
         delete addressToPayer[msg.sender];
 
-        (bool res, ) = receiver.call{ value: payer.amount}("");
-        require(res, "AdAuction::payForAd: Withdrawal failed");
+        (bool res, ) = receiver.call{ value: payer.amount}(""); // convert to payable?
+        if (!res) revert BidWithdrawalFailed();
     }
 
+    function withdraw(address receiver) external onlyOwner {
+        if (block.timestamp <= endAuctionTime) revert AuctionIsNotOverYet();
+        if (address(this).balance == 0) revert NothingToWithdraw();
+
+        (bool res, ) = receiver.call{ value: address(this).balance}("");
+        if (!res) revert OwnerWithdrawalFailed();
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
+    }
 }
--- IAdAuction.sol
+++ IAdAuction.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.0;
 
 interface IAdAuction {
     function payForAd(string calldata _name, string calldata _imageUrl, string calldata _text) payable external;
-    function withdraw(address receiver) external;
+    function withdrawBid(address receiver) external;
+    function withdraw(address receiver) external 
 }
